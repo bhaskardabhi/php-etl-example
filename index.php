d07f2b8836fab87c35e400189aafe31ad74e2ee6
Bhaskar Dabhi <bhaskar@kofluence.com>
2022-11-26 02:21:31 +0530

    Saving the comments which give more description

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once __DIR__.'/vendor/autoload.php';
 
 use Input\CSV;
+use Input\JSON;
 
 use Output\JSON as OutputJSON;
 use Output\CSV as OutputCSV;
@@ -11,8 +12,17 @@ use Transformers\SalaryTransformer;
 SalaryTransformer::register('JSON',OutputJSON);
 SalaryTransformer::register('CSV',OutputCSV);
 
+/**
+ * Convert from CSV to JSON
+ */
 // Loading the input data
-$source = new CSV('employees.csv');
+(new SalaryTransformer(
+    new CSV('employees.csv')
+))->saveAsJSON("employees.json");
 
-// Save the output
-(new SalaryTransformer($source))->saveAsJSON("employees.json");
+/**
+ * Convert from JSON to CSV
+ */
+(new SalaryTransformer(
+    new JSON('employees.json')
+))->saveAsJSON("employees_2.csv");
